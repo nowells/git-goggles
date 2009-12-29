1e89d3aa5045423c11d408b7e252e91574bfd2f3
Nowell Strite <nowell@strite.org>
2009-12-29 16:34:43 -0500

    Prettied up codereview.

--- src/codereview.py
+++ src/codereview.py
@@ -23,39 +23,77 @@ def get_status():
     tags = repo.tags()
 
     branches = filter(lambda x: x != 'master', branches)
-    statuses = []
+    branch_states = []
 
     for branch in branches:
-        parent = 'staging'
-        if branch in ('staging', 'master',):
-            parent = 'master'
+        parent = branch in ('staging', 'master',) and 'master' or 'staging'
 
-        color, status, extra = 'red', '?', ''
+        color, status, review_commits, ahead_commits, behind_commits = 'red', '?', [], [], []
+
+        ahead_commits = repo.git('log', '--pretty=format:"- %s [%h]"', 'origin/%s..origin/%s' % (parent, branch), split=True)
+        behind_commits = repo.git('log', '--pretty=format:"- %s [%h]"', 'origin/%s..origin/%s' % (branch, parent), split=True)
 
         if "%s-codereview" % branch not in tags:
             color, status = 'red', 'new'
-            commits = repo.git('log', '--pretty=format:"- %s [%h]"', 'origin/%s..origin/%s' % (parent, branch), split=True)
-            if commits:
-                extra = '%s commits' % len(commits)
+            review_commits = ahead_commits
         else:
-            commits = repo.git('log', '--pretty=format:"- %s [%h]"', '%s-codereview..origin/%s' % (branch, branch), split=True)
-            if commits:
+            review_commits = repo.git('log', '--pretty=format:"- %s [%h]"', '%s-codereview..origin/%s' % (branch, branch), split=True)
+            if review_commits:
                 color, status = 'red', 'review'
-                extra = '%s commits' % len(commits)
             else:
-                commits = repo.git('log', '--pretty=format:"- %s [%h]"', 'origin/%s..origin/%s' % (parent, branch), split=True)
-                if commits:
+                if ahead_commits:
                     color, status = 'yellow', 'merge'
-                    extra = '%s commits' % len(commits)
                 else:
                     color, status = 'green', 'done'
 
-        statuses.append([status, color, branch, extra])
+        branch_states.append({
+            'status': status,
+            'color': color,
+            'branch': branch,
+            'ahead_commits': ahead_commits,
+            'behind_commits': behind_commits,
+            'review_commits': review_commits,
+            })
+
+        # Track length of longest string for each column
         content_length('status', status)
         content_length('branch', branch)
 
-    for status, color, branch, extra in statuses:
-        print '%s %s %s' % (colored('[ %s ]' % status.upper().rjust(content_length('status')), color), branch.ljust(content_length('branch')), extra and colored('(%s)' % extra, color, attrs=['reverse']) or '')
+    # Print out pretty upper container
+    print '%s-%s-%s-%s-%s' % (
+        '-'.join([ '' for x in range(content_length('status') + len('  ') + 1) ]),
+        '-'.join([ '' for x in range(content_length('branch') + len('  ') + 1) ]),
+        '-'.join([ '' for x in range(16 + 1) ]),
+        '-'.join([ '' for x in range(11 + 1) ]),
+        '-'.join([ '' for x in range(12 + 1) ]),
+        )
+
+    # Print out status for each branch
+    for s in branch_states:
+        review_commits = len(s['review_commits'])
+        ahead_commits = len(s['ahead_commits'])
+        behind_commits = len(s['behind_commits'])
+        color = s['color']
+        branch = s['branch']
+        status = s['status']
+
+        print '%s|%s|%s|%s|%s' % (
+            colored(' %s ' % status.upper().rjust(content_length('status')), color),
+            ' %s ' % branch.ljust(content_length('branch')),
+            colored((' %s unreviewed ' % review_commits).rjust(16), review_commits and color or None, attrs=review_commits and ['reverse'] or []),
+            colored((' %s ahead ' % ahead_commits).rjust(11), ahead_commits and color or None, attrs=ahead_commits and ['reverse'] or []),
+            colored((' %s behind ' % behind_commits).rjust(12), behind_commits and color or None, attrs=behind_commits and ['reverse'] or []),
+            )
+
+    # Print out pretty lower container
+    print '%s-%s-%s-%s-%s' % (
+        '-'.join([ '' for x in range(content_length('status') + len('  ') + 1) ]),
+        '-'.join([ '' for x in range(content_length('branch') + len('  ') + 1) ]),
+        '-'.join([ '' for x in range(16 + 1) ]),
+        '-'.join([ '' for x in range(11 + 1) ]),
+        '-'.join([ '' for x in range(12 + 1) ]),
+        )
+
 
 def complete_review():
     repo = Repository()
